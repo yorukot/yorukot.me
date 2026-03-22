c9053ab6511df4f88dbe6c69908fd68df1d04faa
Yorukot <hi@yorukot.me>
2026-03-22 16:48:03 +0800

    feat: add icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import Icons from "unplugin-icons/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss(),
+      Icons({
+        compiler: "astro",
+      }),
+    ],
   },
 });
